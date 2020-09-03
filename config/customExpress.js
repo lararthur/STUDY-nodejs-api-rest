3e0fd5383b540c4aef1952c62290ef39eef7b82d
polvora-fosforo <contaparacodigo999@gmail.com>
2020-09-03 16:27:32 -0300

    Utilizando bodyParser para fazer o parsing de requisições de um <form> (urlencoded) e de json (raw)

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,13 +1,13 @@
 const express = require('express');
-// o consign vai agrupar as rotas criadas fora do entry point (no caso, este arquivo aqui, o index.js)
-// depois, ele coloca todas as rotas detro do app
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = () => {
     const app = express();
     
-    // primeiro, executamos o consign, depois chamamos a pasta controllers, onde estarão as rotas
-    // e depois, jogá-las no app.
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.json());
+
     consign()
         .include('controllers')
         .into(app);
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,9 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Você está na rota de antedimentos e está realizando um GET'));
+
+    app.post('/atendimentos', (req, res) => {
+        console.log('---réqui bári', req.body);
+
+        res.send('Este carro está sendo roubado, e é monitorado pela car system');
+    });
 }
