10701f804123b747f00488ceae3269d7697353ec
polvora-fosforo <contaparacodigo999@gmail.com>
2020-09-05 11:39:46 -0300

    Criando tabela no servidor

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const customExpress = require('./config/customExpress');
 const conexao = require('./infraestrutura/conexao');
+const Tabelas = require('./infraestrutura/tabelas');
 
 // aqui, conectamos com o bd indicado no arquivo de conexão em infraestrutura
 // e só iremos rodar o servidor de fato após a conexão estar funcionando
@@ -9,6 +10,7 @@ conexao.connect((erro) => {
     } else {
         console.log('conectado com o bd');
 
+        Tabelas.init(conexao);
         const app = customExpress();
 
         app.listen(3000, () => console.log('servidor rodando e girando na porta 3000'));
