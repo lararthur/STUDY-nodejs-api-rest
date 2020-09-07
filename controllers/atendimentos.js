4bae1cb4139e59f2215fc0effa033af85fbc1ae5
polvora-fosforo <contaparacodigo999@gmail.com>
2020-09-07 11:34:50 -0300

    métodos de lista e buscaPorId - verbo GET

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,10 +1,22 @@
 const Atendimento = require('../models/atendimentos');
 
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de antedimentos e está realizando um GET'));
+    // listar todos os atendimentos
+    app.get('/atendimentos', (req, res) => {
+        Atendimento.lista(res);
+    });
 
+    // buscar atendimento
+    app.get('/atendimentos/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+
+        Atendimento.buscaPorId(id, res);
+    });
+
+    // cadastrar atendimentos
     app.post('/atendimentos', (req, res) => {
         const atendimento = req.body;
+
         Atendimento.adiciona(atendimento, res);
     });
 }
