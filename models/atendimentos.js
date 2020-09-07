f3af70b974a2028f1119dd890494a89c803c4ce0
polvora-fosforo <contaparacodigo999@gmail.com>
2020-09-07 10:25:37 -0300

    limpando comentarios

--- models/atendimentos.js
+++ models/atendimentos.js
@@ -3,21 +3,10 @@ const conexao = require('../infraestrutura/conexao');
 
 class Atendimento {
     adiciona(atendimento) {
-        // data do atendimento com o moment
-        // o moment subtitui o new date() padrão do JavaScript
-        const dataCriacao = moment()
-        .format('YYYY-MM-DD HH:MM:SS'); // no .format(), estamos dizendo em que formato o moment vai nos devolver a data.
-
-        // manipulando a data agendada com o moment :)
-        const data = moment(
-            // aqui no primeiro parametro, passamos a data vinda da requisição
-            atendimento.data,
-            // como o moment trabalha com o padrão americano (ao invés de DD/MM/YYYY, ele é MM/DD/YYYY)
-            // então, no segundo parâmetro, especificamos como queremos que o moment leia nossa data
-            'DD/MM/YYYY'
-        ).format('YYYY-MM-DD HH:MM:SS');
-
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS');
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
         const atendimentoDatado = {...atendimento, dataCriacao, data};
+
         const sql = `INSERT INTO Atendimentos SET ?`;
 
         conexao.query(sql, atendimentoDatado, (erro, resultados) => {
